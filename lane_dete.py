694e0d42f8abbc6570bfb27a0232829ef2d7ee55
WilliamWang1994 <chenglong.wang666@gmail.com>
2022-01-20 14:16:09 +0800

    qrcode

--- lane_dete.py
+++ lane_dete.py
@@ -1,31 +1,31 @@
-import cv2 as cv
+import cv2
 import numpy as np
 
 
 def do_canny(frame):
-    # Converts frame to grayscale because we only need the luminance channel for detecting edges - less computationally expensive
-    gray = cv.cvtColor(frame, cv.COLOR_RGB2GRAY) #预处理:讲彩色图变为灰度图
+    gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY) #预处理:讲彩色图变为灰度图
     # Applies a 5x5 gaussian blur with deviation of 0 to frame - not mandatory since Canny will do this for us
-    blur = cv.GaussianBlur(gray, (5, 5), 0) #高斯滤波去噪声
+    blur = cv2.GaussianBlur(gray, (5, 5), 0) #高斯滤波去噪声
     # Applies Canny edge detector with minVal of 50 and maxVal of 150
-    canny = cv.Canny(blur, 50, 150) #canny边缘检测
+    canny = cv2.Canny(blur, 50, 150) #canny边缘检测
     return canny
 
 
 def do_segment(frame):
     # Since an image is a multi-directional array containing the relative intensities of each pixel in the image, we can use frame.shape to return a tuple: [number of rows, number of columns, number of channels] of the dimensions of the frame
     # frame.shape[0] give us the number of rows of pixels the frame has. Since height begins from 0 at the top, the y-coordinate of the bottom of the frame is its height
-    height = frame.shape[0] 
+    height, width = frame.shape[:2]
     # Creates a triangular polygon for the mask defined by three (x, y) coordinates
     polygons = np.array([
-                            [(0, height), (800, height), (380, 290)]
+                            [(0, height), (width, height), (width, int(height/2)), (0, int(height/2))]
                         ])
     # Creates an image filled with zero intensities with the same dimensions as the frame
     mask = np.zeros_like(frame)
     # Allows the mask to be filled with values of 1 and the other areas to be filled with values of 0
-    cv.fillPoly(mask, polygons, 255)
+    cv2.fillPoly(mask, polygons, 255)
     # A bitwise and operation between the mask and frame keeps only the triangular area of the frame
-    segment = cv.bitwise_and(frame, mask)  
+    # cv2.imshow("mask", mask)
+    segment = cv2.bitwise_and(frame, mask)
     return segment
 
 
@@ -47,8 +47,8 @@ def calculate_lines(frame, lines):
         else:
             right.append((slope, y_intercept))
     # Averages out all the values for left and right into a single slope and y-intercept value for each line
-    left_avg = np.average(left, axis = 0)
-    right_avg = np.average(right, axis = 0)
+    left_avg = np.average(left, axis=0)
+    right_avg = np.average(right, axis=0)
     # Calculates the x1, y1, x2, y2 coordinates for the left and right lines
     left_line = calculate_coordinates(frame, left_avg)
     right_line = calculate_coordinates(frame, right_avg)
@@ -75,36 +75,39 @@ def visualize_lines(frame, lines):
     if lines is not None:
         for x1, y1, x2, y2 in lines:
             # Draws lines between two coordinates with green color and 5 thickness
-            cv.line(lines_visualize, (x1, y1), (x2, y2), (0, 255, 0), 5)
+            cv2.line(lines_visualize, (x1, y1), (x2, y2), (0, 255, 0), 5)
     return lines_visualize
 
 
 # The video feed is read in as a VideoCapture object
-cap = cv.VideoCapture("input.mp4")
+cap = cv2.VideoCapture("input.mp4")
 
 while cap.isOpened():
     # ret = a boolean return value from getting the frame, frame = the current frame being projected in the video
     ret, frame = cap.read()
     if not ret:
         break
-    canny = do_canny(frame) #canny边缘检测
-    cv.imshow("canny", canny)
+    canny = do_canny(frame)  # canny边缘检测
+    # cv2.imshow("canny", canny)
     # plt.imshow(frame)
     # plt.show()
-    segment = do_segment(canny)  #手工分割
-    hough = cv.HoughLinesP(segment, 2, np.pi / 180, 100, np.array([]), minLineLength=100, maxLineGap=50) #霍夫变换,确定车道线
-    # Averages multiple detected lines from hough into one line for left border of lane and one line for right border of lane
-    lines = calculate_lines(frame, hough) 
+    segment = do_segment(canny)  # 手工分割
+    # cv2.imshow("segment", segment)
+    # cv2.waitKey(0)
+    hough = cv2.HoughLinesP(segment, 2, np.pi / 180, 100, np.array([]), minLineLength=100, maxLineGap=50)
+    #  霍夫变换,确定车道线
+    lines = calculate_lines(frame, hough)
     # Visualizes the lines
     lines_visualize = visualize_lines(frame, lines) #显示车道线
-    cv.imshow("hough", lines_visualize)
-    # Overlays lines on frame by taking their weighted sums and adding an arbitrary scalar value of 1 as the gamma argument
-    output = cv.addWeighted(frame, 0.9, lines_visualize, 1, 1) #车道和原始图片叠加
+    cv2.imshow("hough", lines_visualize)
+    cv2.waitKey(0)
+    #  Overlays lines on frame by taking their weighted sums and adding an arbitrary scalar value of 1 as the gamma argument
+    output = cv2.addWeighted(frame, 0.9, lines_visualize, 1, 1)  # 车道和原始图片叠加
     # Opens a new window and displays the output frame
-    cv.imshow("output", output)
+    cv2.imshow("output", output)
     # Frames are read by intervals of 10 milliseconds. The programs breaks out of the while loop when the user presses the 'q' key
-    if cv.waitKey(10) & 0xFF == ord('q'):
+    if cv2.waitKey(10) & 0xFF == ord('q'):
         break
 # The following frees up resources and closes all windows
 cap.release()
-cv.destroyAllWindows()
+cv2.destroyAllWindows()
